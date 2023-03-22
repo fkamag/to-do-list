a21e0fdb87a14c13e0a6381238f39e34480a0797
fkamag <fabiokamagian@gmail.com>
2023-03-21 23:51:05 -0300

    feat components

---
+++ to-do-list/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Button(props) {
+  const { children } = props;
+  return (
+    <button>
+      { children }
+    </button>
+  )
+}
+
+export default Button;
--- to-do-list/src/pages/GetStarted.jsx
+++ to-do-list/src/pages/GetStarted.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import Image from '../components/Image.jsx';
+import GetStartedBanner from '../assets/GetStartedBanner.svg';
+import Title from '../components/Title.jsx';
+import Button from '../components/Button.jsx';
+import styles from './GetStarted.module.css';
 
 function GetStarted() {
   return (
-    <h1>GetStarted</h1>
+    <section className={ styles.container }>
+      <Image src={ GetStartedBanner } alt="homem sentado" />
+      <Title title='Get things done with TODo' />
+      <p>
+        Lorem ipsum dolor sit amet,
+        consectetur adipisicing. Maxime,
+        tempore! Animi nemo aut atque,
+        deleniti nihil dolorem repellendus.
+      </p>
+      <Button>
+        Get Started
+      </Button>
+    </section>
   )
 }
 
